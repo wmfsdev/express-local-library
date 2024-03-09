c5721bb645c3b8b137243c4ff9e743f25d3d1dd9
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-03-09 00:07:07 +0000

    install mongoose and add db connection

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+require('dotenv').config()
+
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const specialUsersRouter = require('./routes/special')
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.mongoURL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

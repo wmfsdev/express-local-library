c4072aa28d14e6c752756442f6c7c3251bfd6623
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2024-03-27 20:09:28 +0000

    update GET and POST genre_update function handlers in genre controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -110,12 +110,54 @@ exports.genre_delete_post = asyncHandler(async (req, res, next) => {
   res.redirect("/catalog/genres")
 });
 
+
 // Display Genre update form on GET.
 exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
+  
+  const genre = await Genre.findById(req.params.id)
+
+  if (genre === null) {
+    // No results.
+    const err = new Error("Genre not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("genre_form", {
+    title: "Update Genre",
+    genre: genre,
+  })
 });
 
+
 // Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+exports.genre_update_post = [
+  body("genre", "Genre must not be empty.")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    
+    const errors = validationResult(req);
+
+    const genre = new Genre({
+      name: req.body.genre,
+      _id: req.params.id,
+    });
+
+    if (!errors.isEmpty()) {
+    
+      const genre = await Genre.findById(req.params.id).exec()
+
+      res.render("genre_form", {
+        title: "Update Genre",
+        genre: genre,
+        errors: errors.array(),
+      })
+    } else {
+      const updatedGenre = await Genre.findByIdAndUpdate(req.params.id, genre, {})
+      res.redirect(updatedGenre.url)
+    }
+  })
+]
